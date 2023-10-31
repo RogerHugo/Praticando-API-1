4e61cbc82c7fdfe97ba10a268621f60f040ccc0b
Roger Agostinho <rogerhugo703@gmail.com>
2023-10-31 12:44:30 +0100

    update

--- api/router.php
+++ api/router.php
@@ -1,18 +1,21 @@
 <?php
 function router()
 {
+    echo "entrou na função router<br>";
     $uri = $_SERVER["REQUEST_URI"];
+    echo "$uri <br>";
     switch($uri)
     {
-        case "Praticando-API-1/api/v1/pessoas/":
-            return require __DIR__ . "v1/pessoas/index.php";
-    
+        case "/Praticando-API-1/api/v1/pessoas/ ":
+            echo "index.pphp<br>";
+            //return require __DIR__ . "v1/pessoas/index.php";
+            
         case "api/v1/":
             return require __DIR__ . "api/v1/pessoas/.php";
         case "api/v1/pessoas/store":
             return require __DIR__ . "api/v1/pessoas/store.php";
-        case "api/v1/pessoas/update":
-            return require __DIR__ . "api/v1/pessoas/update.php";
+        case "/Praticando-API-1/api/v1/pessoas/update":
+            return require __DIR__ . "v1/pessoas/update.php";
         case "api/v1/pessoas/delete":
             return require __DIR__ . "api/v1/pessoas/.php";
         default:
